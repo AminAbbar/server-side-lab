c334d1dbbdeccc5cadfed1518e9f5f4fd7cb86b1
Amin <91750157+AminAbbar@users.noreply.github.com>
2024-08-11 12:07:00 +0200

    getting ready

---
+++ Assets/AuthenticationManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using TMPro;
+using System.Threading.Tasks;
+public class AuthenticationManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
