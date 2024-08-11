403babe7a6122673d696ac55c88375c535c338c4
Amin <91750157+AminAbbar@users.noreply.github.com>
2024-08-11 12:26:00 +0200

    setup authentication

--- Assets/AuthenticationManager.cs
+++ Assets/AuthenticationManager.cs
@@ -7,15 +7,77 @@ using TMPro;
 using System.Threading.Tasks;
 public class AuthenticationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    [SerializeField]
+    private  GameObject player;
+    [SerializeField]
+    private GameObject authUI; 
+    private cloudManager cloudManager;
+ 
+ 
+  async void Awake()
     {
+        await UnityServices.InitializeAsync();
+        cloudManager = FindObjectOfType<cloudManager>();
+        SetupEvents();
+
+     
+
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+              try
+              {
+                  await AuthenticationService.Instance.SignInAnonymouslyAsync();
+                  player.SetActive(true);
+                  authUI.SetActive(false);
+              }
+              catch (AuthenticationException ex)
+              {
+                GameObject.Find("error").GetComponent<TMP_Text>().text = ex.Message;
+
+              }
+              catch (RequestFailedException ex)
+              {
+                GameObject.Find("error").GetComponent<TMP_Text>().text = ex.Message;
+    
+              }
+
         
+        }
+        else
+        {
+            player.SetActive(true);
+            authUI.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+  void SetupEvents() 
     {
-        
+        AuthenticationService.Instance.SignedIn += () => 
+        {
+            Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
+   
+
+
+            player.SetActive(true);
+            authUI.SetActive(false);
+        };
+
+        AuthenticationService.Instance.SignInFailed += (err) => 
+        {
+            Debug.LogError(err);
+        };
+
+        AuthenticationService.Instance.SignedOut += () => 
+        {
+            Debug.Log("Player signed out.");
+        };
+
+        AuthenticationService.Instance.Expired += () =>
+        {
+            Debug.Log("Player session could not be refreshed and expired.");
+        };
     }
 }
